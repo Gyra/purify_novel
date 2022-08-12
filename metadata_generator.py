cd3c0d63ec71e1e532de8481537228cf73232e60
Handing <sun.handing@outlook.com>
2022-08-13 00:32:53 +0100

    fix index

--- metadata_generator.py
+++ metadata_generator.py
@@ -62,14 +62,16 @@ def parse_title(title):
     return book, num, attributes
 
 
+location = 0
 for i in ind:
     title = chapters[i].replace('.png', '').replace('pic_test\\', '').replace('pic\\', '')
     book, num, attr = parse_title(title)
-    metadata = {'name': f'洁净文学 #{str(i+1)}',
+    metadata = {'name': f'洁净文学 #{str(location)}',
                 'description': f'符合新时代语言规范的洁净文学选段： 《{book}》第{num}回',
-                'image': ipfs + f'/{str(i+1)}.png',
+                'image': ipfs + f'/{str(location)}.png',
                 'attributes': attr
                 }
-    os.rename(chapters[i], f'pic\\{str(i+1)}.png')
-    with open(f'metadata\\{str(i+1)}', 'w', encoding='utf-8') as f:
+    os.rename(chapters[i], f'pic\\{str(location)}.png')
+    with open(f'metadata\\{str(location)}', 'w', encoding='utf-8') as f:
         json.dump(metadata, f, ensure_ascii=False)
+    location += 1
